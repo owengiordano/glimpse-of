f416663601cee6c6cac4667e78e7430cf86cc2ea
Owen Giordano <owengiord@gmail.com>
2023-06-15 00:15:35 -0400

    clean up frontend and install react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import 'bootsrtap/dist/bootsrtap/css/bootsrtap.min.css';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
