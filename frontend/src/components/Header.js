0a2f6d9a287d7617ad47915ddc1a590b263f2c5c
Owen Giordano <owengiord@gmail.com>
2023-06-15 21:04:57 -0400

    create favicon and add logo to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 import { BsSpotify } from 'react-icons/bs';
+import { ReactComponent as Logo } from '../images/logo.svg';
+
 //import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navbarStyle = {
-  backgroundColor: '#dae8e5',
+  backgroundColor: '#191825',
 };
 
 const Header = ({ title }) => {
   return (
-    <Navbar style={navbarStyle} variant="light">
+    <Navbar style={navbarStyle} className="mt-0" variant="light">
       <Container>
-        <Navbar.Text className="mx-auto">Glimpse of</Navbar.Text>
+        <Navbar.Text className="mx-auto">
+          <Logo
+            alt={'Glimpsify'}
+            style={{ maxWidth: '20rem', maxHeight: '4rem' }}
+          />
+        </Navbar.Text>
         <Navbar.Text></Navbar.Text>
       </Container>
     </Navbar>
